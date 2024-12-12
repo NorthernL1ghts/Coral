a0a222a75588f75727d20ae47169856129b65ec3
NorthernL1ghts <NorthernL1ghts@gmail.com>
2024-12-12 22:25:02 +0000

    Updated Windows + Linux build scripts

--- Scripts/Linux/Build.sh
+++ Scripts/Linux/Build.sh
@@ -2,9 +2,14 @@
 
 cd ../../
 
-premake5 gmake2 --file=premake5-native.lua
-premake5 vs2022 --file=premake5-managed.lua
+# Use the correct path to the premake executable
+vendor/bin/premake/premake5 gmake2 --file=premake5-native.lua
+vendor/bin/premake/premake5 vs2022 --file=premake5-managed.lua
+
+# Build the project using all but one CPU core
 make -j$(($(nproc) - 1))
+
+# Build the .NET solution
 dotnet build CoralManaged.sln
 
 cd ./Scripts/Linux/
