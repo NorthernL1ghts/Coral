a6ac2a0faaaccab555b1015be68a2164588089a2
NorthernL1ghts <NorthernL1ghts@gmail.com>
2024-12-26 17:14:09 +0000

    Added PAUSE / wait step to both scripts

--- Scripts/Linux/Build.sh
+++ Scripts/Linux/Build.sh
@@ -6,10 +6,19 @@ cd ../../
 vendor/bin/premake/premake5 gmake2 --file=premake5-native.lua
 vendor/bin/premake/premake5 vs2022 --file=premake5-managed.lua
 
+# Pause and wait
+read -p "Press any key to continue..."
+
 # Build the project using all but one CPU core
 make -j$(($(nproc) - 1))
 
+# Pause and wait
+read -p "Press any key to continue..."
+
 # Build the .NET solution
 dotnet build CoralManaged.sln
+
+# Pause and wait
+read -p "Press any key to continue..."
 
 cd ./Scripts/Linux/
